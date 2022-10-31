69aaba706293d1a3e1d0537ee389664c04a4bc4c
giapl <69382475+giapl@users.noreply.github.com>
2022-10-31 21:00:09 +0700

    bài nhập số tÀI KHOẢN
    nhập số tài khoản cho trước , rút ra xem có được không

---
+++ Untitled1.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+ using namespace std;
+ #define tk 100 
+ int main()
+ {
+ 	cout<<"nhap so du tk :";
+ 	int a;
+ 	cin>>a;
+ 	if(a<=tk)
+	 {
+ 		cout<<"rut duoc thanh cong"<<endl;
+	 }
+ 	else{
+ 		cout<<"rut tien that bai"<<endl;
+	 }
+	return 0;
+  } 
